a272166e625da0d938198ebb7c2e68c8d661271e
John <JohnPhamDeveloper@hotmail.com>
2019-12-02 20:27:45 -0800

    Changed home to use proper cookie route checking

--- help/src/App.js
+++ help/src/App.js
@@ -10,55 +10,54 @@ import { setIsLoggedIn } from "./redux/actions/userActions.js";
 import { connect } from "react-redux";
 
 const options = {
-	withCredentials : true,
-}
+  withCredentials: true
+};
 
 const App = ({ isLoggedIn, dispatch }) => {
-	
-	const logOutUser = () => {
-		axios.get("/logout", options).then(res => {
-			console.log(res);
-		});
-		// dispatch(setIsLoggedIn(false));
-		// window.location.href= "/";
-	};
-	return (
-		<div className="App">
-			<div
-				id="navlink-div"
-				className="flex-display-row justify-content-space-evenly padding-2-p border-bottom"
-			>
-				<NavLink to="/">Home </NavLink>
-				<NavLink to="/writereview">Write a review </NavLink>
-				<NavLink to="/settings"> Settings </NavLink>
-				<NavLink to="/business"> Business </NavLink>
-				{!isLoggedIn && <NavLink to="/login"> Login </NavLink>}
-				{!isLoggedIn && <NavLink to="/join"> Join </NavLink>}
+  const logOutUser = () => {
+    axios.get("/logout", options).then(res => {
+      console.log(res);
+    });
+    // dispatch(setIsLoggedIn(false));
+    // window.location.href= "/";
+  };
+  return (
+    <div className="App">
+      <div
+        id="navlink-div"
+        className="flex-display-row justify-content-space-evenly padding-2-p border-bottom"
+      >
+        <NavLink to="/">Home </NavLink>
+        <NavLink to="/writereview">Write a review </NavLink>
+        <NavLink to="/settings"> Settings </NavLink>
+        <NavLink to="/business"> Business </NavLink>
+        {!isLoggedIn && <NavLink to="/login"> Login </NavLink>}
+        {!isLoggedIn && <NavLink to="/join"> Join </NavLink>}
 
-				<button
-					className="btn btn-primary"
-					onClick={() => {
-						logOutUser();
-					}}
-				>
-					Log Out
-				</button>
-			</div>
-			<Switch>
-				<Route path="/writereview" />
-				<Route path="/settings" />
-				<Route path="/business" />
-				<Route path="/login" component={Login} />
-				<Route path="/join" component={Signup} />
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            logOutUser();
+          }}
+        >
+          Log Out
+        </button>
+      </div>
+      <Switch>
+        <Route path="/writereview" />
+        <Route path="/settings" />
+        <Route path="/business" />
+        <Route path="/login" component={Login} />
+        <Route path="/join" component={Signup} />
 
-				<Route exact path="/" component={Home} />
-			</Switch>
-		</div>
-	);
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </div>
+  );
 };
 
 const mapStateToProps = state => ({
-	isLoggedIn: state.userReducer.isLoggedIn
+  isLoggedIn: state.userReducer.isLoggedIn
 });
 
 export default connect(mapStateToProps)(App);
