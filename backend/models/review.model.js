309c87c8c54c8f644da3eb8afde1b1160f050e1d
Daisy Sun <itsdaisysun@gmail.com>
2019-12-04 20:30:02 -0800

    review schema

---
+++ backend/models/review.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    username:{
+        required: true,
+        type: String,
+    },
+
+    text:{
+        required: true,
+        type: String,
+    },
+
+    restaurantID:{
+        required: true,
+        type: [Number],
+    }
+});
+
+module.exports = mongoose.model('review', reviewSchema);
