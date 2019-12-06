504dcf426bf3f977b68a0bd819cdae11b7f60e5c
aadityac1511 <achokshi@mail.sfsu.edu>
2019-12-06 13:47:35 -0800

    Every page is checked in the cookie for the user. Changed the title to the app name

--- help/src/pages/Reviews.jsx
+++ help/src/pages/Reviews.jsx
@@ -3,15 +3,42 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { Redirect, Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { setIsLoggedIn, setUsername } from "../redux/actions/userActions";
+import axios from "axios";
+import {
+    setIsLoggedIn,
+    setUsername,
+    setIsBusiness
+} from "../redux/actions/userActions";
 import { setCurrentBusiness } from "../redux/actions/businessActions";
 
 const options = {
     withCredentials: true
 };
 
+const Reviews = ({
+    dispatch,
+    isBusiness,
+    isLoggedIn,
+    username,
+    currentBusiness
+}) => {
+    React.useEffect(() => {
+        axios.post("/auth/cookies", options).then(res => {
+            console.log(res);
+            if (res.data.message === "Successfully authenticated") {
+                dispatch(setIsBusiness(res.data.user.isBusiness));
+                dispatch(setUsername(res.data.user.username));
+                dispatch(setIsLoggedIn(true));
+            }
+        });
+        axios.post("/restaurant", options).then(res => {
+            console.log(res);
+        })
+    }, []);
 
-const Reviews = ({ dispatch, isBusiness, isLoggedIn, username, currentBusiness }) => {
+
+
+
 
     return (
         <div>
@@ -30,8 +57,10 @@ const Reviews = ({ dispatch, isBusiness, isLoggedIn, username, currentBusiness }
             </div>
             <div>
                 {currentBusiness ? (
-                    <div>You are currently reviewing {currentBusiness}</div>)
-                 : (<div>You are not currently reviewing any business</div>)}
+                    <div>You are currently reviewing <b>{currentBusiness}</b></div>
+                ) : (
+                    <div>You are not currently reviewing any business</div>
+                )}
             </div>
         </div>
     );
@@ -42,6 +71,7 @@ const mapStateToProps = state => ({
     isLoggedIn: state.userReducer.isLoggedIn,
     isBusiness: state.userReducer.isBusiness,
     currentBusiness: state.businessReducer.currentBusiness,
+    username : state.userReducer.username,
 });
 
 export default withRouter(connect(mapStateToProps)(Reviews));
