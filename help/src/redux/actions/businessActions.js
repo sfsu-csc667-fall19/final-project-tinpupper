e7409c1d4157e8969274a73f5f07b92fdb65ed54
John <JohnPhamDeveloper@hotmail.com>
2019-12-07 01:44:04 -0800

    .map BugFix

--- help/src/redux/actions/businessActions.js
+++ help/src/redux/actions/businessActions.js
@@ -16,21 +16,19 @@ export const listBusinesses = () => (dispatch, getState) => {
     });
 };
 
-
-
 export const setId = _id => ({
   type: 'BUSINESS_SET_ID',
   _id,
 });
 
-export const setNewBusiness = newBusiness=> ({
+export const setNewBusiness = newBusiness => ({
   type: 'BUSINESS_SET_NEW_BUSINESS',
   newBusiness,
 });
 
-export const setIsRedirect = isRedirect=> ({
+export const setIsRedirect = isRedirect => ({
   type: 'SET_REDIRECT',
-  isRedirect
+  isRedirect,
 });
 
 export const setCurrentBusiness = currentBusiness => ({
@@ -39,16 +37,17 @@ export const setCurrentBusiness = currentBusiness => ({
 });
 
 export const updateBusiness = () => (dispatch, getState) => {
-  const {_id , newBusiness} = getState().businessReducer;
- // const _id = getState().notesReducer._id;
+  const { _id, newBusiness } = getState().businessReducer;
+  // const _id = getState().notesReducer._id;
   //const newNote  = getState().newReducer.newNote;
 
-  axios.get(`/update?_id=${_id}&businesses=${newBusiness}`)
-  .then((res) => {
-    dispatch(setNewBusiness(''));
-    dispatch(setId(''));
-    dispatch(listBusinesses());
-    dispatch(setCurrentBusiness());
-  })
-  .catch(console.log);
-}
+  axios
+    .get(`/update?_id=${_id}&businesses=${newBusiness}`)
+    .then(res => {
+      dispatch(setNewBusiness(''));
+      dispatch(setId(''));
+      dispatch(listBusinesses());
+      dispatch(setCurrentBusiness());
+    })
+    .catch(console.log);
+};
