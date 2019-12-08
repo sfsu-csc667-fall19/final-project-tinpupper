143e1ddd4936fca4bd1027b61a9147590634d70b
LightProgrammerZM <34073626+LightProgrammerZM@users.noreply.github.com>
2019-12-08 15:35:45 -0800

    Added more input fields to Business Page

--- help/src/pages/Business.jsx
+++ help/src/pages/Business.jsx
@@ -24,10 +24,27 @@ class Business extends React.Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label> 
-                    Name:
+                   Restaurant Name:
                     <input type="text" value={this.state.value} onChange={this.handleChange} />
                 </label>
                 <input type="submit" value="Submit" />
+                <br />
+
+                <label>
+                    Type: 
+                    <input type="text"  value={this.state.value} onChange={this.handleChange} />   
+                </label> 
+                <input type="submit" value="Submit" />
+                <br />
+
+                <label>
+                    Description: 
+                    <input type="text"  value={this.state.value} onChange={this.handleChange} />   
+                </label> 
+                <input type="submit" value="Submit" />
+                <br />
+
+            <input type = "submit" value = "Submit"/>
             </form>
         );
     }
