de9f6fe952f307d4fdefff716a1d2c71f9ff7eee
John <JohnPhamDeveloper@hotmail.com>
2019-12-10 17:46:29 -0800

    Other reviews show up for reviews page

--- help/src/pages/Reviews.jsx
+++ help/src/pages/Reviews.jsx
@@ -34,6 +34,29 @@ const styleReviewForm = {
 
 const Reviews = ({ dispatch, isBusiness, isLoggedIn, username, currentBusiness }) => {
   const [reviewText, setReviewText] = React.useState('');
+  const [loadedReviews, setLoadedReviews] = React.useState(false);
+  const [reviews, dispatchReviews] = React.useReducer((reviews, { type, value }) => {
+    switch (type) {
+      case 'add':
+        return [...reviews, ...value];
+      case 'clear':
+        return [];
+      default:
+        return reviews;
+    }
+  }, []);
+
+  React.useEffect(() => {
+    /*********************************************************
+     * JOHN (12/10/2019):
+     * Scroll down to find function
+     *********************************************************/
+    console.log('business changed');
+    console.log(currentBusiness);
+    if (currentBusiness) {
+      fetchReviews();
+    }
+  }, [currentBusiness]);
 
   const onSubmitReview = () => {
     const restaurantId = currentBusiness._id;
@@ -63,6 +86,83 @@ const Reviews = ({ dispatch, isBusiness, isLoggedIn, username, currentBusiness }
     setReviewText(e.target.value);
   };
 
+  /*********************************************************
+   * JOHN (12/10/2019):
+   * Fetch all reviews for current restaurant
+   *********************************************************/
+  const fetchReviews = async () => {
+    console.log('inside fetch reviews');
+    console.log(currentBusiness);
+    if (!currentBusiness.reviewIds) return;
+    console.log('lol');
+
+    const reviews = [];
+
+    // Get all reviews for current restaurants
+    for (let i = 0; i < currentBusiness.reviewIds.length; i++) {
+      const review = {
+        userId: '',
+        username: '',
+        text: '',
+      };
+
+      console.log('logging');
+      try {
+        await axios.get(`/review/${currentBusiness.reviewIds[i]}`).then(res => {
+          console.log(res.data);
+          review.userId = res.data.userId;
+          review.text = res.data.text;
+          // dispatchReviews({ type: 'add', value: res.data });
+        });
+        await axios.get(`/user/${review.userId}`).then(res => {
+          console.log(res.data.users.username);
+          review.username = res.data.users.username;
+        });
+
+        reviews.push(review);
+      } catch (e) {
+        alert('Failed to get reviews');
+        console.log(e);
+      }
+    }
+
+    dispatchReviews({ type: 'add', value: reviews });
+    setLoadedReviews(true);
+  };
+
+  /*********************************************************
+   * JOHN (12/10/2019):
+   * Displays review onto page
+   *********************************************************/
+  const renderReviews = () => {
+    console.log(reviews);
+    if (!reviews || !reviews.length || reviews.length <= 0) return;
+
+    console.log('accessed');
+    console.log(reviews);
+    return reviews.map(review => {
+      console.log(review.text);
+      console.log(review.userId);
+      return (
+        <div
+          key={`${review.text} AND ${review.userId}`}
+          style={{
+            marginTop: '30px',
+            marginBottom: '30px',
+            backgroundColor: 'orange',
+            width: '50%',
+            padding: '30px',
+            margin: '30px auto',
+          }}
+        >
+          <h3>{review.username}</h3>
+          <p>{review.text}</p>
+          <p>{review.userId}</p>
+        </div>
+      );
+    });
+  };
+
   return (
     <div>
       <div>
@@ -83,6 +183,10 @@ const Reviews = ({ dispatch, isBusiness, isLoggedIn, username, currentBusiness }
           <div>You are not currently reviewing any business</div>
         )}
       </div>
+      {currentBusiness && !loadedReviews && <h1 style={{ marginTop: '30px' }}>Loading...</h1>}
+
+      <div>{renderReviews()}</div>
+
       <div style={styleReviewForm}>
         <textarea
           onChange={onReviewChange}
